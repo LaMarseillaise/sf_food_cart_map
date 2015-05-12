307072ced96910a8fcb50c8cd1b9af6710a9c827
Andur Carr <carr.andur@gmail.com>
2015-05-12 15:57:20 -0700

    Add basic geolocation

--- public/js/carts.js
+++ public/js/carts.js
@@ -2,11 +2,27 @@ var map;
 
 function initialize() {
   var sf = new google.maps.LatLng(37.761, -122.441);
+  map = new google.maps.Map(document.getElementById('map-canvas'));
 
-  map = new google.maps.Map(document.getElementById('map-canvas'), {
-    center: sf,
-    zoom: 13
+  var givenLocation = new google.maps.Marker({
+    map: map,
+    position: sf
   });
+
+  // Geolocation; code from Google.
+  if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = new google.maps.LatLng(position.coords.latitude,
+                                       position.coords.longitude);
+
+      givenLocation.setPosition(pos);
+      map.setCenter(pos);
+      map.setZoom(17);
+    }, function() {
+      map.setCenter(sf);
+      map.setZoom(13);
+    });
+  }
 
   $.getJSON("https://data.sfgov.org/resource/rqzj-sfat.json", function(data) {
     $.each(data, function(key, cart) {

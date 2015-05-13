4c82255d310c77c82017ac8ccf0e918b34900eb3
Andur Carr <carr.andur@gmail.com>
2015-05-12 21:29:26 -0700

    Add minor styling

--- public/js/carts.js
+++ public/js/carts.js
@@ -1,39 +1,49 @@
-var map;
+(function(){
+  var map;
 
-function initialize() {
-  var sf = new google.maps.LatLng(37.761, -122.441);
-  map = new google.maps.Map(document.getElementById('map-canvas'));
+  function initialize() {
+    var sf = new google.maps.LatLng(37.761, -122.441);
+    map = new google.maps.Map(document.getElementById('map-canvas'), {
+      center: sf,
+      zoom: 18
+    });
 
-  var givenLocation = new google.maps.Marker({
-    map: map,
-    position: sf
-  });
+    // Geolocation; code from Google.
+    if(navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        var pos = new google.maps.LatLng(position.coords.latitude,
+                                         position.coords.longitude);
+        map.setCenter(pos);
+        map.setZoom(18);
+      }, function() {
+        map.setCenter(sf);
+        map.setZoom(13);
+      });
+    }
 
-  // Geolocation; code from Google.
-  if(navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      var pos = new google.maps.LatLng(position.coords.latitude,
-                                       position.coords.longitude);
+    $.getJSON("https://data.sfgov.org/resource/rqzj-sfat.json", function(data) {
+      $.each(data, function(key, cart) {
+        var coords = new google.maps.LatLng(cart.latitude, cart.longitude);
+        var marker = new google.maps.Marker({
+          position: coords,
+          map: map,
+          title: cart.applicant
+        });
 
-      givenLocation.setPosition(pos);
-      map.setCenter(pos);
-      map.setZoom(17);
-    }, function() {
-      map.setCenter(sf);
-      map.setZoom(13);
+        var infowindow = new google.maps.InfoWindow({
+            content:  "<h3>" + cart.applicant + "</h3>" +
+                      "<p><strong>Food:</strong> "    + cart.fooditems.replace(/:/g, ',') + "</p>" +
+                      "<p><strong>Type:</strong> "    + cart.facilitytype + "</p>" +
+                      "<p><strong>Address:</strong> " + cart.address      + "</p>" +
+                      "<p><strong>Status:</strong> "  + cart.status       + "</p>"
+        });
+
+        google.maps.event.addListener(marker, 'click', function() {
+          infowindow.open(map,marker);
+        });
+      });
     });
   }
 
-  $.getJSON("https://data.sfgov.org/resource/rqzj-sfat.json", function(data) {
-    $.each(data, function(key, cart) {
-      var coords = new google.maps.LatLng(cart.latitude, cart.longitude);
-      var marker = new google.maps.Marker({
-        position: coords,
-        map: map,
-        title: cart.applicant
-      });
-    });
-  });
-}
-
-google.maps.event.addDomListener(window, 'load', initialize);
+  google.maps.event.addDomListener(window, 'load', initialize);
+})();
